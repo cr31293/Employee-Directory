c97b88a9ef33b9f9ca32604f3ac813fc48cd00ea
Chris Reed <cr31293@gmail.com>
2020-10-30 21:50:32 -0400

    Updates

---
+++ precache-manifest.fe03d54d4d6bde09679174c69a277f0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24785223510c36a64bef7fef8748f94d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ca3ca3989ba6428d8ce",
+    "url": "/static/js/2.57ee6957.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.57ee6957.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f085f8aaa42ac31d0a97",
+    "url": "/static/js/main.13d57a30.chunk.js"
+  },
+  {
+    "revision": "c74b6dcb725f329d7bfe",
+    "url": "/static/js/runtime-main.403e0656.js"
+  }
+]);
