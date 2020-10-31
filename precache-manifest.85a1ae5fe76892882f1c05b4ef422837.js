ad2dd63c6e1c80c507d21c3024105e08369c126b
Chris Reed <cr31293@gmail.com>
2020-10-30 22:44:58 -0400

    Updates

---
+++ precache-manifest.85a1ae5fe76892882f1c05b4ef422837.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fd0acb2a490364f5f1e4eb2d0bd9d8e",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "6ca3ca3989ba6428d8ce",
+    "url": "/my-app/static/js/2.57ee6957.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/my-app/static/js/2.57ee6957.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4bb2fc81c3db72a1032",
+    "url": "/my-app/static/js/main.0b84ca7c.chunk.js"
+  },
+  {
+    "revision": "b3505c3d477b4db463a1",
+    "url": "/my-app/static/js/runtime-main.958628b4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.479ed42a6bf572d441b7081cf3f85bca.js"
+  "/my-app/precache-manifest.85a1ae5fe76892882f1c05b4ef422837.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
